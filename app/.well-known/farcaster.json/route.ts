02fe8239a84a5ad6d7e15e1d4d12211b21a04a85
minhhung <ps19acc@gmail.com>
2024-12-26 22:14:26 +0700

    json

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,24 @@
+export async function GET() {
+    const config = {
+      accountAssociation: {
+        header:
+          'eyJmaWQiOjE5MDkxNSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGJlNjdDN0RENDlkRDQ1YTFhNTlDZjdDNEU5MzI2ZUNhMEQyMzkwNDcifQ',
+        payload: 'eyJkb21haW4iOiJldGg0bGlmZS52ZXJjZWwuYXBwIn0',
+        signature:
+          'MHgzNmJkZTFkODY4YjBlNjZmZDQxOTYzZTE1MjJjNTk0NzFhYmYyZTY0MDlkZGMwZTMzODJmMjlkMjcxOWI1Njg4NDdkMWJkOThkY2M3NDAzY2FhMTliNjg1NjVjN2FmMTI3YTExZGY3OWI1NTBhNjMxMTZkZWNlN2JkMGQ2ODM2NjFi',
+      },
+      frame: {
+        version: '1',
+        name: 'Airdrop',
+        iconUrl: 'https://eth4life.vercel.app/logo.png',
+        homeUrl: 'https://eth4life.vercel.app',
+        imageUrl: 'https://eth4life.vercel.app/logo.png',
+        buttonTitle: 'Launch Frame',
+        splashImageUrl: 'https://eth4life.vercel.app/logo.png',
+        splashBackgroundColor: '#eeccff',
+        webhookUrl: 'https://eth4life.vercel.app/api/webhook',
+      },
+    };
+  
+    return Response.json(config);
+  }
